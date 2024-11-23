ec7360b38cf45efe1bb70102ada9ca079a402d06
jinsu723 <firend.723@daum.net>
2024-11-24 01:42:04 +0900

    241124 1시 42분 java복습 후 과제 제출

---
+++ week01/src/homework/Printlnfo.java
@@ -0,0 +1,7 @@
+package homework;
+
+public class Printlnfo {
+	public static void main(String[] args) {  // 메인 함수
+		System.out.println("길진수 / 27살");  // 이름과 나이를 출력하고 줄을 바꾼다.
+	}
+}
---
+++ week01/src/homework/Printlnfo2.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class Printlnfo2 {
+	public static void main(String[] args) {  // 메인 함수
+		System.out.print("2024년 11월 12일");  // 연도와 날짜를 출력하고 줄을 바꾸지 않는다.
+		System.out.print(" 23시 39분");  // 시간을 출력하고 줄을 바꾸지 않는다.
+	}
+}
